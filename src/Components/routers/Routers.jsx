c2df796ccf267f91dd08f736faacd6ec602b27a0
GuthiManeendra <maneendraguthi@gmail.com>
2025-12-28 00:14:10 +0530

    add Home page with routing and service components

--- src/Components/routers/Routers.jsx
+++ src/Components/routers/Routers.jsx
@@ -1,6 +1,17 @@
-export const Routers =()=>{
+import { Route, Routes } from "react-router-dom"
+import Home from "../../pages/Home"
+
+
+export const Routers=()=>{
     return(
         <>
+     
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/home" element={<Home/>}/>
+             
+
+        </Routes>
         
         </>
     )
